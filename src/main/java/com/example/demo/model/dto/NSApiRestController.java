6945994e8e377c34d853fd529e15ee33f75bbda6
alexscamg <alejandrosanchezsk8@gmail.com>
2024-03-29 12:12:48 +0000

    saco datos extras de la API

--- src/main/java/com/example/demo/model/dto/NSApiRestController.java
+++ src/main/java/com/example/demo/model/dto/NSApiRestController.java
@@ -28,21 +28,19 @@ public class NSApiRestController {
 
 	@Autowired
 	private RestTemplate restTemplate;
-	
+
 	String subscriptionKey = SubscriptionKey.subscriptionKey;
-	
 
 	List<String> titles = new ArrayList<>();
-	
+
 	@GetMapping("/about-us")
 	public String muestraAbout() {
-		
+
 		return "about-us/about-us";
 	}
 
 	public List<Map<String, Object>> sacaIDDisruptions() {
 		String apiUrl = "https://gateway.apiportal.ns.nl/reisinformatie-api/api/v3/disruptions?isActive=true";
-		
 
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
@@ -53,6 +51,7 @@ public class NSApiRestController {
 			ResponseEntity<String> responseEntity = restTemplate.exchange(apiUrl, HttpMethod.GET, requestEntity,
 					String.class);
 			String jsonResponse = responseEntity.getBody();
+			// System.out.println(jsonResponse);
 
 			// Convertir el JSON a un Map
 			ObjectMapper objectMapper = new ObjectMapper();
@@ -61,7 +60,9 @@ public class NSApiRestController {
 					});
 
 			// Imprimir el Map resultante
-			// System.out.println(resultList);
+			//System.out.println("Map resultante: ");
+			//System.out.println(resultList);
+			//System.out.println("Termina sMap resultante: ");
 			return resultList;
 		} catch (HttpStatusCodeException e) {
 
@@ -76,55 +77,126 @@ public class NSApiRestController {
 		return null;
 	}
 
-	public Map<String, String> extraerIds(List<Map<String, Object>> list) {
-		
+	// -----------------------------------------
+	public Map<String, String> extraerIdsCompletos(List<Map<String, Object>> list) {
+
 		Map<String, String> idTituloMap = new HashMap<>();
+		
+		
+		Map<String, String> listaDatosId = new HashMap<>();
+		
 
 		for (Map<String, Object> mapa : list) {
 			String id = (String) mapa.get("id");
 			String title = (String) mapa.get("title");
+			String type = (String) mapa.get("type");
+			String description = null;
+			Map<String, Object> situation;
+			Map<String, Object> cause;
+			String situationLabel = null;
+			String causeLabel = null;
+			List<Map<String, Object>> publicationSections2 = (ArrayList) mapa.get("publicationSections");
+			List<Map<String, Object>> timespans = (ArrayList) mapa.get("timespans");
 			
-			if (id.length()<9) {
-			//	System.out.println("Id limpiado: "+id);
+			
+			System.out.println("id: " + id);
+			System.out.println("type: " + type);
+			
+	
+	        for (Map<String, Object> section : publicationSections2) {
+	            // Obtener el mapa 'consequence' dentro del mapa actual
+	            Map<String, Object> consequence = (Map<String, Object>) section.get("consequence");
+
+	            // Obtener la descripción del mapa 'consequence'
+	            description = (String) consequence.get("description");
+
+	            // Imprimir la descripción
+	            System.out.println("Descripciónnn: " + description);
+	        }
+	 
+	        
+	        for (Map<String, Object> section : timespans) {
+	            // Obtener el mapa 'situation' dentro del mapa actual
+	             situation = (Map<String, Object>) section.get("situation");
+	             
+	            // Obtener el mapa 'cause' dentro del mapa actual
+	             cause = (Map<String, Object>) section.get("cause");
+	            
+	         // Obtener la descripción del mapa 'label'
+	             situationLabel = (String) situation.get("label");
+	            
+	            // Obtener la descripción del mapa 'label'
+	            causeLabel = (String) cause.get("label");
+
+	            // Imprimir la descripción
+	            System.out.println("Situatuion Label: " + situationLabel);
+	            // Imprimir la descripción
+	            System.out.println("Cause Label: " + causeLabel);
+	        }
+			
+			
+
+			if (id.length() < 9) {
+				// System.out.println("Id limpiado: "+id);
 				idTituloMap.put(id, title);
+				listaDatosId.put("situationLabel", situationLabel);
+				listaDatosId.put("type", type);
+				listaDatosId.put("causeLabel", causeLabel);
+				listaDatosId.put("description", description);
 			}
-			
+
 		}
 
 		return idTituloMap;
 	}
 
-	//Ruta por defecto
+//-----------------------------------------
+
+	public Map<String, String> extraerIds(List<Map<String, Object>> list) {
+
+		Map<String, String> idTituloMap = new HashMap<>();
+
+		for (Map<String, Object> mapa : list) {
+			String id = (String) mapa.get("id");
+			String title = (String) mapa.get("title");
+
+			if (id.length() < 9) {
+				// System.out.println("Id limpiado: "+id);
+				idTituloMap.put(id, title);
+			}
+
+		}
+
+		return idTituloMap;
+	}
+
+	// Ruta por defecto
 	@GetMapping("/")
 	public String sacaGEOJsonDefecto(Model model) {
-		
 
 		// Obtener el mapa de IDs y títulos
-		Map<String, String> idTitulosMap = extraerIds(sacaIDDisruptions());
-	    
+		Map<String, String> idTitulosMap = extraerIdsCompletos(sacaIDDisruptions());
+
 		model.addAttribute("datos", idTitulosMap);
 
 		return "home/index";
 	}
-	
-   //Ruta con indice para mostrar vias
-	@GetMapping("/{index}")
-	public String sacaGEOJson(@PathVariable(required = false) String index, Model model) {
 
+	// Ruta con indice para mostrar vias
+	@GetMapping("/{index}")
+	public String sacaGEOJson(@PathVariable(required = false) int index, Model model) {
 
 		// Obtener el mapa de IDs y títulos
-		Map<String, String> idTitulosMap = extraerIds(sacaIDDisruptions());
-	   
+		Map<String, String> idTitulosMap = extraerIdsCompletos(sacaIDDisruptions());
 
 		model.addAttribute("datos", idTitulosMap);
 
 		String apiUrl;
+		String indexString = String.valueOf(index);
 
-		if (index != null) {
-			System.out.println("Con Indice");
+		if (indexString != null) {
+			//System.out.println("Con Indice");
 			apiUrl = "https://gateway.apiportal.ns.nl/Spoorkaart-API/api/v1/storingen/" + index;
-
-			
 
 			HttpHeaders headers = new HttpHeaders();
 			headers.setContentType(MediaType.APPLICATION_JSON);
@@ -140,8 +212,8 @@ public class NSApiRestController {
 				JsonNode rootNode = objectMapper.readTree(jsonResponse);
 				JsonNode payloadNode = rootNode.get("payload");
 
-				System.out.println("geoJason");
-				System.out.println(payloadNode);
+				//System.out.println("geoJason");
+				//System.out.println(payloadNode);
 
 				model.addAttribute("content", payloadNode);
 
