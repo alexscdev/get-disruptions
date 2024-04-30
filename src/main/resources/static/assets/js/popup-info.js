3fbef166799cb8e59c729ef0fcdc2a0b92da495e
alexscamg <alejandrosanchezsk8@gmail.com>
2024-04-30 17:44:40 +0200

    version 2.0

--- src/main/resources/static/assets/js/popup-info.js
+++ src/main/resources/static/assets/js/popup-info.js
@@ -1,7 +1,10 @@
 var infoButton = document.createElement('button');
 infoButton.setAttribute('type', 'button');
-infoButton.setAttribute('id', 'infoCustomButton');
+infoButton.setAttribute('id', 'customButton');
 infoButton.classList.add('btn', 'btn-dark', 'position-absolute', 'top-0', 'end-0', 'm-3', 'mt-5'); 
+
+infoButton.style.setProperty('top', '10px', 'important');
+
 
 
 var infoIcon = document.createElement('i');
@@ -11,22 +14,29 @@ infoIcon.setAttribute('aria-hidden', 'true');
 
 infoButton.appendChild(infoIcon);
 
-
 map.getContainer().appendChild(infoButton);
 
 
-infoButton.addEventListener('click', function () {
-    
-    var infoButtonPosition = infoButton.getBoundingClientRect();
-    
-    var popup = document.getElementById('popup');
-    if (popup.style.display === 'none' || popup.style.display === '') {
-        
-        popup.style.left = '10px'; 
-        popup.style.top = infoButtonPosition.top + 'px';
-        popup.style.display = 'block';
-    } else {
-        popup.style.display = 'none';
-    }
-});
+var currentURL = window.location.href;
+
+
+if (currentURL.includes("http://localhost:8080/") && currentURL !== "http://localhost:8080/") {
+
+    infoButton.addEventListener('click', function () {
+        var infoButtonPosition = infoButton.getBoundingClientRect();
+        var popup = document.getElementById('popup');
+        if (popup.style.display === 'none' || popup.style.display === '') {
+            popup.style.left = '10px'; 
+            popup.style.top = infoButtonPosition.top + 'px';
+            popup.style.display = 'block';
+        } else {
+            popup.style.display = 'none';
+        }
+    });
+} else {
+
+    infoButton.style.display = 'none';
+}
+
+
 
